e590069ab0a029fc09eabc36806ef4a74861e4e1
thesaavn <saavnsr@gmail.com>
2025-01-07 23:35:32 +0530

    finished exercises 3.9-3.11

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # fullstackopen part3
+
+## https://fullstackopen-part3-phonebook.fly.dev/
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 let persons = [
@@ -31,8 +32,10 @@ morgan.token('body', (request, response) => {
   }
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.static('dist'))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World</h1>')
@@ -80,8 +83,8 @@ app.post('/api/persons', (request, response) => {
   response.json(newPerson)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 app.listen(PORT, () => {
-  console.log('Server running on ', PORT)
+  console.log('Server running on port ', PORT)
 })
